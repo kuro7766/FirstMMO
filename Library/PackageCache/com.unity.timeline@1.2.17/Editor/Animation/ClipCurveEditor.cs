759b1d8264576d0d61c7488bf4250ff9c4ce1bff
kuro <7445841+kuro7766@user.noreply.gitee.com>
2021-03-27 09:22:14 +0800

    remove LIbrary folder

--- Library/PackageCache/com.unity.timeline@1.2.17/Editor/Animation/ClipCurveEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b9d35fb12498ce4f8915b737ea1de42e970399636c9421c81fd678cf1c9e040f
-size 12600
--- Library/webgl_cache/linkresult_wasm_9C5457B5E77C4348D238CC61A7280FE0/build.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af023aa3212cb0ba511f03c655b454bde9f2df4610dc14e8dadb3b338b4ed886
-size 1462939
